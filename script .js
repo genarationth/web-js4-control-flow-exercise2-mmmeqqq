32f26ca107c1c10d5755fd2f4b08cdf652db7534
mmmeqqq <bew.bussayakorn@gmail.com>
2024-05-14 11:22:46 +0700

    done

---
+++ script .js
@@ -0,0 +1,25 @@
+let dayNumber = 1 ;
+
+switch (true) {
+    case dayNumber == 0 :
+        console.log('Sunday');
+        break;
+        case dayNumber == 1 :
+        console.log('Monday');
+        break;
+        case dayNumber == 2 :
+        console.log('Tueday');
+        break;
+        case dayNumber == 3 :
+        console.log('Wedday');
+        break;
+        case dayNumber == 4 :
+        console.log('Thursday');
+        break;
+        case dayNumber == 5 :
+        console.log('Friday');
+        break;
+        case dayNumber == 6 :
+        console.log('Saturday');
+        break;
+}
